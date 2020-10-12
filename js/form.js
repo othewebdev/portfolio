273f1a74fee5877b2ab909ed14b55432455ff0cf
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2020-10-12 16:18:44 -0500

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,10 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+})

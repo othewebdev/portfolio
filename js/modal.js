a43d7370d0ec8775cbcace84cb8b48bd95cc8fbd
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2020-10-19 08:47:50 -0500

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -5,6 +5,10 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("img1");
 var img2 = document.getElementById("img2");
 var img3 = document.getElementById("img3");
+var img4 = document.getElementById("img4");
+var img5 = document.getElementById("img5");
+var img6 = document.getElementById("img6");
+var img7 = document.getElementById("img7");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function() {
@@ -33,6 +37,49 @@ img3.onclick = function() {
     modalImg.style.width = "80vw";
     modalImg.style.objectFit = "contain";
     captionText.innerHTML = this.alt;
+}
+
+if (img4, img5, img6, img7) {
+  
+    img4.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.style.border = 'none';
+        modalImg.style.height = "80vh";
+        modalImg.style.width = "80vw";
+        modalImg.style.objectFit = "contain";
+        captionText.innerHTML = this.alt;
+    }
+
+    img5.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.style.border = 'none';
+        modalImg.style.height = "80vh";
+        modalImg.style.width = "80vw";
+        modalImg.style.objectFit = "contain";
+        captionText.innerHTML = this.alt;
+    }
+
+    img6.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.style.border = 'none';
+        modalImg.style.height = "80vh";
+        modalImg.style.width = "80vw";
+        modalImg.style.objectFit = "contain";
+        captionText.innerHTML = this.alt;
+    }
+
+    img7.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.style.border = 'none';
+        modalImg.style.height = "80vh";
+        modalImg.style.width = "80vw";
+        modalImg.style.objectFit = "contain";
+        captionText.innerHTML = this.alt;
+    }
 }
 
 
